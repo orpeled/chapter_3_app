ed4d0e6b47f14c1d5cb1710132fa39722a523271
Or Peled <orppeled@gmail.com>
2012-11-11 16:50:03 -0600

    Initial Copy For GitHub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chapter3App::Application.config.secret_token = 'e1364ab9258b3f06fccb7b9735f00127a800517d8349a9f1e1e0fa88db70af1b4a6b33760c3a77cef443928ca84891b2c7d428e5a30b7c72ea4ebbde40257bbf'
